88f9bc8083d2fc2375650e99f03fd77c5864a62a
Alexander Faustino <faustinoalexander46@gmail.com>
2026-08-30 21:47:56 -0500

    Agrega funciones de calculo de multa,estado del prestamo y muestra de la tabla de multas

--- semana03/librearia/app/src/main/java/com/faustino/librearia/Prestamo.kt
+++ semana03/librearia/app/src/main/java/com/faustino/librearia/Prestamo.kt
@@ -1,4 +1,6 @@
 package com.faustino.libreria
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 data class Prestamo(
     val titulo: String,
@@ -10,3 +12,28 @@ data class Prestamo(
 )
 
 const val MULTA_POR_DIA = 1.50
+
+val FORMATO_FECHA: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+fun calcularMultaTotal(diasAtraso: Int): Double {
+    var acumulado = 0.0
+    for (dia in 1..diasAtraso) {
+        acumulado += MULTA_POR_DIA
+    }
+    return acumulado
+}
+
+fun estadoPrestamo(diasAtraso: Int): String {
+    return if (diasAtraso > 0) "Devuelto con $diasAtraso dia(s) de atraso" else "Devuelto a tiempo"
+}
+
+fun mostrarTablaMultas(fechaDevolucionTexto: String, diasAtraso: Int) {
+    val fechaDevolucion = LocalDate.parse(fechaDevolucionTexto, FORMATO_FECHA)
+    println("Dia\tFecha\t\tMulta/Dia\tAcumulado")
+    var acumulado = 0.0
+    for (dia in 1..diasAtraso) {
+        acumulado += MULTA_POR_DIA
+        val fechaDia = fechaDevolucion.plusDays(dia.toLong())
+        println(String.format("%d\t%s\tS/ %.2f\tS/ %.2f", dia, fechaDia.format(FORMATO_FECHA), MULTA_POR_DIA, acumulado))
+    }
+}
