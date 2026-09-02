eed72bfd74f75cc31a5bb6f4b0d24866bf7d7abd
alexanderfaustino <faustinoalexander46@gmail.com>
2026-09-02 17:24:50 -0500

    Agrega encabezado con jerarquia tipografica

--- semana03/Lab03RegistroProducto/app/src/main/java/com/faustino/lab03registroproducto/MainActivity.kt
+++ semana03/Lab03RegistroProducto/app/src/main/java/com/faustino/lab03registroproducto/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.faustino.lab03registroproducto
-
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.faustino.lab03registroproducto.ui.theme.Lab03RegistroProductoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab03RegistroProductoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PantallaRegistro(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +32,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun PantallaRegistro(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Nuevo producto",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text(
+            text = "Completa los datos y presiona Agregar.",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.outline
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PantallaRegistroPreview() {
     Lab03RegistroProductoTheme {
-        Greeting("Android")
+        PantallaRegistro()
     }
 }
