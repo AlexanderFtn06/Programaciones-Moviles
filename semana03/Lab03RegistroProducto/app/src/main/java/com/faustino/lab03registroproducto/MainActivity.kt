8a2967e831d69b49df3be83fb6bfa31bb8d719a3
alexanderfaustino <faustinoalexander46@gmail.com>
2026-09-02 17:39:15 -0500

    Agrega campos de ingreso con estado

--- semana03/Lab03RegistroProducto/app/src/main/java/com/faustino/lab03registroproducto/MainActivity.kt
+++ semana03/Lab03RegistroProducto/app/src/main/java/com/faustino/lab03registroproducto/MainActivity.kt
@@ -1,4 +1,7 @@
 package com.faustino.lab03registroproducto
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import android.os.Bundle
@@ -8,10 +11,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,21 +41,63 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PantallaRegistro(modifier: Modifier = Modifier) {
+
+    var nombre by remember { mutableStateOf("") }
+    var precio by remember { mutableStateOf("") }
+    var cantidad by remember { mutableStateOf("") }
+    var mostrarResumen by remember { mutableStateOf(false) }
+
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
+
         Text(
             text = "Nuevo producto",
             style = MaterialTheme.typography.headlineSmall
         )
+
         Text(
-            text = "Completa los datos y presiona Agregar.",
+            text = "Completa los datos y presiona Agregar",
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.colorScheme.outline
         )
-        Spacer(modifier = Modifier.height(24.dp))
+
+        Spacer(
+            modifier = Modifier.height(24.dp)
+        )
+
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = {nombre = it },
+            label = { Text("Nombre del producto")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(
+            modifier = Modifier.height(16.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            OutlinedTextField(
+                value = precio,
+                onValueChange = { precio = it },
+                label = { Text("Precio (S/)")},
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(
+                modifier = Modifier.width(16.dp)
+            )
+            OutlinedTextField(
+                value = cantidad,
+                onValueChange = { cantidad = it },
+                label = { Text("Cantidad") },
+                modifier = Modifier.weight(1f)
+            )
+        }
     }
 }
 
