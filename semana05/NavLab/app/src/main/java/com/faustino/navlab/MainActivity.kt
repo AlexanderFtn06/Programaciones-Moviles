730aac1912657915e2f23c8b23d23867264b261b
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-21 22:20:24 -0500

    semana05-NavLab: Implementa  AppNavigation(NavHost) con las 4 rutas y conecta en MainActivity

--- semana05/NavLab/app/src/main/java/com/faustino/navlab/MainActivity.kt
+++ semana05/NavLab/app/src/main/java/com/faustino/navlab/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.faustino.navlab.navigation.AppNavigation
 import com.faustino.navlab.ui.theme.NavLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NavLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
     }
 }
