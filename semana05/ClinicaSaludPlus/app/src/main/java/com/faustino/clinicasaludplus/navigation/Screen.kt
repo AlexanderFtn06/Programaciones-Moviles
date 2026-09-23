1a45ba42a69fd096375860826a1e9fb9f20981cd
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-22 20:55:10 -0500

    Actividad1-S6:Crea Screen.kt con rutas (home, doctorProfile, scheduleAppointment, confirmation, myAppointments, medicalHistory)

---
+++ semana05/ClinicaSaludPlus/app/src/main/java/com/faustino/clinicasaludplus/navigation/Screen.kt
@@ -0,0 +1,20 @@
+package com.faustino.clinicasaludplus.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen(route = "home")
+    object MyAppointments : Screen(route = "myAppointments")
+    object MedicalHistory : Screen(route = "medicalHistory")
+
+    object DoctorProfile : Screen(route = "doctorProfile/{doctorId}") {
+        fun createRoute(doctorId: Int): String = "doctorProfile/$doctorId"
+    }
+
+    object ScheduleAppointment : Screen(route = "scheduleAppointment/{doctorId}") {
+        fun createRoute(doctorId: Int): String = "scheduleAppointment/$doctorId"
+    }
+
+    object Confirmation : Screen(route = "confirmation/{doctorId}/{fecha}/{hora}") {
+        fun createRoute(doctorId: Int, fecha: String, hora: String): String =
+            "confirmation/$doctorId/$fecha/$hora"
+    }
+}
