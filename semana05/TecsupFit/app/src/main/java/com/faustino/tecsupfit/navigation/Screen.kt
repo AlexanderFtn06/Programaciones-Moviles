2a191864a495e4c098656043b02227203c3863a7
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-23 10:16:43 -0500

    Actividad1-S6-2: Agregar Screen.kt con rutas selladas y parámetro claseId

--- semana05/TecsupFit/app/src/main/java/com/faustino/tecsupfit/navigation/Screen.kt
+++ semana05/TecsupFit/app/src/main/java/com/faustino/tecsupfit/navigation/Screen.kt
@@ -1,2 +1,20 @@
 package com.faustino.tecsupfit.navigation
 
+sealed class Screen(val route: String) {
+
+    object Home : Screen("home")
+
+    object DetalleClase : Screen("detalle_clase/{claseId}") {
+        fun createRoute(claseId: Int) = "detalle_clase/$claseId"
+    }
+
+    object Confirmacion : Screen("confirmacion/{claseId}") {
+        fun createRoute(claseId: Int) = "confirmacion/$claseId"
+    }
+
+    object Reservas : Screen("reservas")
+
+    object Rutinas : Screen("rutinas")
+
+    object Perfil : Screen("perfil")
+}
